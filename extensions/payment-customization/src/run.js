6381359baa4dbe594bdee5168cc69600a47fb8f9
Anastacia <anastacia.glagoleva@overdose.digital>
2024-02-28 18:00:24 +1000

    added metafield

--- extensions/payment-customization/src/run.js
+++ extensions/payment-customization/src/run.js
@@ -14,23 +14,23 @@ const NO_CHANGES = {
 
 /**
 */
-const TRIGGER_PRODUCT_VARIANT_ID = 'gid://shopify/ProductVariant/40810851532866';
-
-/**
-*/
 const PAYMENT_METHOD_TO_HIDE = 'Afterpay';
 
 /**
+* Main function to run the operation.
 * @param {RunInput} input
 * @returns {FunctionRunResult}
 */
 export function run(input) {
-  const isTriggerProductInCart = input.cart.lines.some(line => 
-    line.merchandise.__typename === 'ProductVariant' &&
-    line.merchandise.id === TRIGGER_PRODUCT_VARIANT_ID
-  );
+  const isMetafieldValueOne = input.cart.lines.some(line => {
+    if (line.merchandise.__typename === 'ProductVariant') {
+      const metafieldValue = line.merchandise.product.metafield?.value;
+      return metafieldValue === "1";
+    }
+    return false; 
+  });
 
-  if (isTriggerProductInCart) {
+  if (isMetafieldValueOne) {
     const paymentMethod = input.paymentMethods.find(method => 
       method.name === PAYMENT_METHOD_TO_HIDE
     );
